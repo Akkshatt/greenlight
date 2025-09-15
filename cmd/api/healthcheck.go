fb9825ecc7ebf30d3c0f3c827c61f3d38a73bb1c
Akkshatt <111251147+Akkshatt@users.noreply.github.com>
2025-09-16 03:22:07 +0530

    Added project files

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -7,24 +7,17 @@ func (app *application) healthcheckHandler( w http.ResponseWriter,r *http.Reques
 	// fmt.Fprintln(w,"status: available")
 	// fmt.Fprintf(w,"enviroment: %s\n",app.config.env)
 	// fmt.Fprintf(w,"version:%s\n",version)
-	data := map[string]string{
-		"status": "available",
-		"environment": app.config.env,
-		"version": version,
-	}
-	// js := `{"status" : "available","environment": %q, "version" : %q}`
-	// js = fmt.Sprintf(js, app.config.env, version)
-	// w.Header().Set("Content-Type","application/json")
-	
-	// w.Write([]byte(js))
-	err := app.writeJSON(w, http.StatusOK, data, nil)
-	if err != nil{
-		app.logger.Println(err)
-		http.Error(w,"the server encountered a problem and could not process your request",http.StatusInternalServerError)
-		return
-	}
-// 	js = append(js, '\n')
-// 	w.Header().Set("Content-Type", "application/json")
+	env := envelope{
+"status": "available",
+"system_info": map[string]string{
+"environment": app.config.env,
+"version": version,
+},
+}
 
-//  w.Write(js)
+	err := app.writeJSON(w, http.StatusOK, env, nil)
+if err != nil {
+// Use the new serverErrorResponse() helper.
+app.serverErrorResponse(w, r, err)
+}
 }
--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -2,34 +2,50 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/Akkshatt/greenlight/internals/data"
 	"net/http"
 	"time"
-	"greenlight/internals/data"
 )
-func(app *application) createMovieHandler(w http.ResponseWriter,r *http.Request){
-	fmt.Fprintf(w,"create a new movie")
-}
-func (app *application) showMovieHandler(w http.ResponseWriter,r *http.Request){
-	id, err := app.readIDParam(r)
-    if err != nil{
-		http.NotFound(w,r)
+
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+
+	var input struct {
+		Title   string       `json:"title"`
+		Year    int          ` json:"year"`
+		Runtime data.Runtime `json:"runtime"`
+		Genres  []string     `json:"genres"`
+	}
+
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+
 		return
 	}
 
+	fmt.Fprintf(w, "%+v\n", input)
+}
 
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil {
+		app.notFoundResponse(w, r)
+
+		return
+	}
 
 	movie := data.Movie{
-		ID: id,
+		ID:        id,
 		CreatedAt: time.Now(),
-		Title: "Casablanca",
-		Runtime: 102,
-		Genres: []string{"drama", "romance", "war"},
-		Version: 1,
-}
-err = app.writeJSON(w,http.StatusOK,movie,nil)
-if err != nil{
- app.logger.Println(err)
- http.Error(w,"the serveer encountered a problem and could not process your request ",http.StatusInternalServerError)
-}
-	
+		Title:     "Casablanca",
+		Runtime:   102,
+		Genres:    []string{"drama", "romance", "war"},
+		Version:   1,
+	}
+	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
+	if err != nil {
+		// Use the new serverErrorResponse() helper.
+		app.serverErrorResponse(w, r, err)
+	}
 }
